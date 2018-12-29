b57e41e02ef093e5d1d957c3e1b2f4e46d211e1c
yoel <yoelzeitoun08@gmail.com>
2018-12-29 21:31:58 +0200

    Add weather option by user input and a table with detail of the weather

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { MapWrapper } from './Map';
-import { fetchWeather } from './weatherService';
+import { fetchWeather, fetchWeatherByCountry } from './weatherService';
 
 const iconUrl = id => `http://openweathermap.org/img/w/${id}.png`;
 class App extends Component {
@@ -10,24 +9,29 @@ class App extends Component {
     super(props);
     this.state = {
       position: [32.0853, 34.7818],
+      cityName: "",
       icon: '',
       description: '',
       temperature: 0,
-
       layers: [],
+      inputUserCountry: "",
+      inputUserCity: "",
+      arrWeatherData: []
     };
   }
 
   componentDidMount() {
     const [lat, lng] = this.state.position;
     fetchWeather(lat, lng).then(res => {
-      this.setState({ ...res });
+      const arrUpdated = this.getUpdatedArray(res.currentElementDetails);
+      this.setState({ ...res, arrWeatherData: arrUpdated });
     });
   }
 
   onMapClick = async ({ latlng: { lat, lng } }) => {
     fetchWeather(lat, lng).then(res => {
-      this.setState({ ...res });
+      const arrUpdated = this.getUpdatedArray(res.currentElementDetails);
+      this.setState({ ...res, arrWeatherData: arrUpdated});
     });
     this.setState({ position: [lat, lng] });
   };
@@ -44,15 +48,57 @@ class App extends Component {
       this.setState(prevState => ({ layers: [...prevState.layers, value] }));
     }
   };
+
+  onChangeCountry = e => {
+    this.setState({inputUserCountry: e.target.value})
+    console.log(this.state.inputUserCountry);
+  }
+  onChangeCity = e => {
+    this.setState({inputUserCity: e.target.value})
+    console.log(this.state.inputUserCity);
+  }
+
+  searchByPlace = () => {
+    fetchWeatherByCountry(this.state.inputUserCountry, this.state.inputUserCity).then(res => {
+      const arrUpdated = this.getUpdatedArray(res.currentElementDetails);
+      this.setState({ ...res, inputUserCountry :"", inputUserCity:"", arrWeatherData: arrUpdated});
+    });
+  }
+
+  getUpdatedArray = (data) => {
+    const newArray = this.state.arrWeatherData;
+    newArray.push(data);
+    return newArray;
+  }
+
   render() {
     const {
       position,
       description,
       icon,
       temperature,
-
+      cityName,
       layers,
+      arrWeatherData
     } = this.state;
+    const rowTables = arrWeatherData.map((weather, index) => {
+      return (
+          <tr key = {index}>
+            <td data-title="City Name">{weather.cityName}</td>
+            <td data-title="Degree">{weather.currentTemp}</td>
+            <td data-title="Icon">{<img
+                alt={`${description} icon`}
+                className={'iconSmall'}
+                src={iconUrl(weather.icon)}
+                />}</td>
+            <td data-title="Description">{weather.description}</td>
+            <td data-title="Humidity">{weather.humidity}</td>
+            <td data-title="Pressure">{weather.pressure}</td>
+          </tr>
+        
+      )
+    })
+
     return (
       <div className="App">
         <header>
@@ -70,6 +116,7 @@ class App extends Component {
           <section className={'action-panel'}>
             <section className={'weather-data border card'}>
               <h2>Weather Data</h2>
+              <p>{`${cityName}`}</p>
               <p>{`${description}`}</p>
               <p>{`${temperature}`} &deg; Celsisus</p>
               <img
@@ -117,7 +164,34 @@ class App extends Component {
                 </section>
               </div>
             </section>
+            <section className={"card border input-container"}>
+              <div className={"input-user"}>
+                 <span className="spanInput">country:</span><input value={this.state.inputUserCountry} onChange={this.onChangeCountry} 
+                    className="input" id={"countryInput"}/>
+                  <span className="spanInput">city:</span><input value={this.state.inputUserCity} required={true}
+                     onChange={this.onChangeCity} className="input" id={"cityInput"}/>
+                  <button className="btn" onClick={this.searchByPlace}>Search</button>
+                </div>
+            </section>
           </section>
+          <div className={"info-container"}>
+
+          <table className="responsive-table">
+            <thead>
+              <tr>
+                <th scope="col">City name</th>
+                <th scope="col">Degree</th>
+                <th scope="col">Icon</th>
+                <th scope="col">Description</th>
+                <th scope="col">Humidity</th>
+                <th scope="col">Pressure</th>
+              </tr>
+            </thead>
+            <tbody>
+              {rowTables}
+            </tbody>
+          </table>
+        </div>
         </main>
       </div>
     );
--- src/weatherService.js
+++ src/weatherService.js
@@ -17,12 +17,45 @@ export const fetchWeather = async (lat, lng) => {
       }`
     );
     const weatherData = response.data;
+    console.log(weatherData);
     const currentTemp = temperatureConverter(weatherData.main.temp);
     const { description, icon } = weatherData.weather[0];
+    const cityName = weatherData.name;
+    const pressure = weatherData.main.pressure;
+    const humidity = weatherData.main.humidity;
     return {
       temperature: currentTemp,
       icon,
       description,
+      cityName,
+      currentElementDetails: {cityName, currentTemp, icon, description, pressure, humidity}
+    };
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const fetchWeatherByCountry = async (country="", city="") => {
+  try {
+    const response = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${
+        config.apiKey
+      }`
+    );
+    const weatherData = response.data;
+    const currentTemp = temperatureConverter(weatherData.main.temp);
+    const { description, icon } = weatherData.weather[0];
+    const cityName = weatherData.name;
+    const coord = weatherData.coord;
+    const pressure = weatherData.main.pressure;
+    const humidity = weatherData.main.humidity;
+    return {
+      temperature: currentTemp,
+      icon,
+      description,
+      position: coord,
+      cityName,
+      currentElementDetails: {cityName, currentTemp, icon, description, pressure, humidity}
     };
   } catch (err) {
     console.log(err);
