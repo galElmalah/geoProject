5c18083a266b6aec29682ba66bddb8a235bcf1b3
Gal Elmalah <gale@wix.com>
2019-01-01 11:41:29 +0200

    Updates

---
+++ precache-manifest.80842db309d2cd2222f70f85b16a6660.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff0b3fb1927ccf4553f1",
+    "url": "/geoProject/static/js/runtime~main.ff0b3fb1.js"
+  },
+  {
+    "revision": "098776fe4c82a3d955d3",
+    "url": "/geoProject/static/js/main.098776fe.chunk.js"
+  },
+  {
+    "revision": "0fd8756e9217ebcfacec",
+    "url": "/geoProject/static/js/1.0fd8756e.chunk.js"
+  },
+  {
+    "revision": "098776fe4c82a3d955d3",
+    "url": "/geoProject/static/css/main.989c5f55.chunk.css"
+  },
+  {
+    "revision": "7a7eb20059d2aa2305e1eef530d89f7f",
+    "url": "/geoProject/index.html"
+  }
+];
